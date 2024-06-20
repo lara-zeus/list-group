8338c4135b81ee8139ece9c8298deccfbfebc6c8
Ash Monsh <wh7r.com@gmail.com>
2024-06-20 12:25:22 +0300

    Update list.blade.php

--- resources/views/tables/list.blade.php
+++ resources/views/tables/list.blade.php
@@ -15,7 +15,7 @@
                     @endphp
                 @endif
                 <li x-data>
-                    <a x-tooltip.raw="{{ $item['tooltip'] }}"
+                    <a x-tooltip.raw="{{ $item['tooltip'] ?? '' }}"
                         class="flex items-center space-x-2 rtl:space-x-reverse"
                         target="{{ (isset($item['isNewTab']) && $item['isNewTab']) ? '_blank' : '_self' }}"
                         href="{{ $item['url'] }}"
